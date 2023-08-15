964d91b5a4b3cd185f2ce41ac04378fd97c4b01d
Matheus <matheusvictor1292@gmail.com>
2023-08-15 20:22:32 -0300

    Aula DS 15/08

---
+++ Models/Funcionario.cs
@@ -0,0 +1,45 @@
+using Aula03Colecoes.Models.Enuns;
+
+namespace Aula03Colecoes.Models
+{
+    public class Funcionario
+    {   // public int Id { get; set; }
+        //  int = tipo, id = nome
+        public int Id { get; set; }
+        public string Nome { get; set; } = "";
+        public string Cpf { get; set; } = string.Empty;
+        public DateTime DataAdmissao { get; set; }
+        public decimal Salario { get; set; }
+        public TipoFuncionarioEnum TipoFuncionario { get; set; }
+        public void ReajustarSalario()
+        {
+            Salario = Salario + (Salario * 10/100);
+        }
+
+        public string ExibirPeriodoExperiencia()
+        {
+            string periodoExperiencia = string.Format("Período de Experiência: {0} até {1}", DataAdmissao, DataAdmissao.AddMonths(3));
+            return periodoExperiencia;
+        }
+
+        public decimal CalcularDescontoVT(decimal percentual)
+        {
+            decimal desconto = this.Salario * percentual/100;
+            return desconto;
+        }
+
+        private int ContarCaracteres(string dado)
+        {
+            return dado.Length;
+        }
+
+        public bool ValidarCpf()
+        {
+            if(ContarCaracteres(Cpf) == 11)
+                return true;
+            else
+                return false;
+                
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Aula03Colecoes.Models;
+using Aula03Colecoes.Models.Enuns;
+
+namespace Aula03Colecoes
+{
+    class Program
+    {
+        static List<Funcionario> lista = new List<Funcionario>(); 
+        static void Main(string[] args)
+        {      
+            //Programação sempre aqui      
+            Console.WriteLine("Hello, World 2DS!");
+        }
+
+        public static void CriarLista()
+        {
+            Funcionario f1 = new Funcionario();
+            f1.Id = 1;
+            f1.Nome = "Neymar";
+            f1.Cpf = "12345678910";
+            f1.DataAdmissao = DateTime.Parse("01/01/2000");
+            f1.Salario = 100.000M;
+            f1.TipoFuncionario = TipoFuncionarioEnum.CLT;
+            lista.Add(f1);
+        }
+    }
+}
+
+// See https://aka.ms/new-console-template for more information
